faba99723fab174d56fa355980ab0a4bef30a254
fin <fin+github@xbhd.org>
2012-09-01 03:00:09 +0200

    allow relabeling of gexf, provide renderer that still makes it work

--- graph/views.py
+++ graph/views.py
@@ -2,16 +2,21 @@ from djangorestframework.views import View
 from djangorestframework.renderers import BaseRenderer, DocumentingHTMLRenderer, JSONRenderer, XMLRenderer
 from models import Network, Relation, Entity
 import networkx
+from networkx.readwrite.gexf import relabel_gexf_graph
+
+from gexfsupport import render_gexf
 
 class OurRenderer(XMLRenderer):
     media_type = 'xml/gexf'
     def render(self, obj=None, media_type=None):
-        print self.view.response.__class__
         self.view.response.headers['Access-Control-Allow-Origin'] = '*'
         return obj
 
-def sanitize(d):
-    return dict([(k,v,) for k,v in d.iteritems() if not k.startswith('_') and not k=='data'])
+def sanitize(d, pk):
+    x = dict([(k,v,) for k,v in d.iteritems() if not k.startswith('_') and not k=='data'])
+    x['label'] = x['title'] if 'title' in x else x['slug']
+    x['id'] = pk
+    return x
 
 class NetworkGexfView(View):
     renderers = (DocumentingHTMLRenderer,OurRenderer,)
@@ -21,11 +26,12 @@ class NetworkGexfView(View):
         entities = set()
 
         for relation in n.relation_set.all():
+            graph.add_edge(relation.source.pk, relation.target.pk, **sanitize(relation.__dict__, relation.pk))
             entities.add(relation.source)
             entities.add(relation.target)
         for entity in entities:
-            graph.add_node(entity.pk, **sanitize(entity.__dict__))
-        for relation in n.relation_set.all():
-            graph.add_edge(relation.source.pk, relation.target.pk, **sanitize(relation.__dict__))
+            graph.add_node(entity.pk, **sanitize(entity.__dict__, entity.pk))
 
-        return '\n'.join(networkx.generate_gexf(graph))
+        graph2 = relabel_gexf_graph(graph)
+
+        return '\n'.join(render_gexf(graph2))
